03fee23d5410e617add056b180ae94335875a142
Valirio <junior.braatz@hotmail.com>
2021-04-03 13:55:16 -0300

    finis frist leson commit

--- src/routes.js
+++ src/routes.js
@@ -6,17 +6,17 @@ const views = __dirname + "/views/";
 const profile ={
     name: "Valirio",
     avatar: "https://avatars.githubusercontent.com/u/16794865?v=4",
-    "monthlyBudget":3000,
-    "daysPerWeek":5,
-    "hoursPerDay":5,
-    "vacationPerYear":4
+    "monthly-budget":3000,
+    "days-per-week": 5,
+    "hours-per-day": 5,
+    "vacation-per-year": 4
 
 }
 
 routes.get('/', (request, response) => response.render(views + "index"));
 routes.get('/job', (request, response) => response.render(views + "job"));
 routes.get('/job/edit', (request, response) => response.render(views + "job-edit"));
-routes.get('/profile', (request, response) => response.render(views + "profile", { profile: profile}));
+routes.get('/profile', (request, response) => response.render(views + "profile", {profile}));
 
 
 
