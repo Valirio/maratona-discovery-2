4d413a5bf75f93d9d1690fb6ad20fda0f1987c6d
Valirio <junior.braatz@hotmail.com>
2021-04-08 17:58:08 -0300

    last commit

--- src/controllers/JobController.js
+++ src/controllers/JobController.js
@@ -1,63 +1,58 @@
 const Job = require('../model/Job')
-const JobUtils = require('../utils/JobUtils');
-const Profile = require('../model/Profile');
+const JobUtils = require('../utils/JobUtils')
+const Profile = require('../model/Profile')
 
 module.exports = {
-    create(request, response){
-        return response.render("job");
+    create(request, response) {
+      return res.render("job")
     },
 
     async save(request, response) {
-        await Job.create({
-          name: request.body.name,
-          "daily-hours": request.body["daily-hours"], 
-          "total-hours": request.body["total-hours"], 
-          created_at: Date.now()
-        });       
+      await Job.create({
+        name: request.body.name,
+        "daily-hours": request.body["daily-hours"], 
+        "total-hours": request.body["total-hours"], 
+        created_at: Date.now()
+      });     
 
-        return response.redirect('/');
+      return response.redirect('/')
     },
 
     async show(request, response) {
-        const jobId = request.params.id;
-        const jobs = await Job.get();
-  
-        const job = jobs.find(job => Number(job.id) === Number(jobId));
-  
-        if (!job) {
-          return response.send('Job not found!');
-        }
-  
-        const profile = await Profile.get();
-  
-        job.budget = JobUtils.calculateBudget(job, profile["value-hour"])
-  
-        return response.render("job-edit", { job })
+      const jobId = request.params.id
+      const jobs = await Job.get()
+
+      const job = jobs.find(job => Number(job.id) === Number(jobId))
+
+      if (!job) {
+        return response.send('Job not found!')
+      }
+
+      const profile = await Profile.get()
+
+      job.budget = JobUtils.calculateBudget(job, profile["value-hour"])
+
+      return response.render("job-edit", { job })
     },
 
     async update(request, response) {
-      const jobId = request.params.id;
+      const jobId = request.params.id
 
       const updatedJob = {
         name: request.body.name,
         "total-hours": request.body["total-hours"], 
         "daily-hours": request.body["daily-hours"], 
-      }      
+      }  
+      await Job.update(updatedJob, jobId)
 
-      await Job.update(updatedJob, jobId);
-
-      response.redirect('/');
+      response.redirect('/job/' + jobId)
     },
-    
+
     async delete(request, response) {
-        const jobId = request.params.id
-  
-        await Job.delete(jobId)
-        
-        return response.redirect('/')
-    },
+      const jobId = request.params.id
 
-    edit(request, response){
-        return response.render("job-edit")
+      await Job.delete(jobId)
+      
+      return response.redirect('/')
     }
-}
+  }    
